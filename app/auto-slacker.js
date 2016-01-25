331aa703f1576b006bd4986643689028ff489235
svoig <harrisonccole@gmail.com>
2016-01-24 23:37:04 -0800

    Started a listChannels method to test another API call. Got the channels, but can't figure out how to display them. Tried to use generators for async, but haven't figured it out yet

--- app/auto-slacker.js
+++ app/auto-slacker.js
@@ -1,23 +1,60 @@
+"use strict";
+
 const request = require('request');
 
+const token = require('./slack-data').token;
+
 var AutoSlacker = (function(){
-	function AutoSlacker(org, token) {
+	function AutoSlacker() {
 
 		this.endPoint = "https://slack.com/api";
+		//Needed for most API calls
+		this.tokenParam = "?token=" + token + "&pretty=1";
+
+		this.channels = {};
+
+		this.options = {
+				method: "GET"
+		};
 
 		this.test = function() {
-			this.urlEnd = "/api.test";
+			const self = this;
 
-			this.options = {
-				url: this.endPoint + this.urlEnd,
-				method: "GET"
-			};
+			this.options.url = this.endPoint + "/api.test";
 
-			request(this.options, function(err, res, body) {
+			request(self.options, function(err, res, body) {
 				console.log(body);
-			})
+			});
 
 		};
+
+		this.listChannels = function() {
+			
+			const self = this;
+
+			this.options.url = this.endPoint + "/channels.list" + this.tokenParam;
+
+			request(self.options, function(err, res, body) {
+				console.log(JSON.parse(body).ok);
+			//Convert the JSON response and store the channels
+				self.channels = JSON.parse(body).channels;
+			});
+
+			let channels = Object.keys(self.channels);
+
+			let chanNames = channels.forEach(function(key) {
+				return key.name;
+			});
+
+			return chanNames;
+		}
+
+		this.listChannels = function() {	
+		}
+
+		/* this.sendMessage = function(channel, token, text) {
+
+		} */
 
 	}
 
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,14 +1,38 @@
+"use strict";
+
 var express = require('express');
 var router = express.Router();
 var AS = require('../auto-slacker.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	
+
   AS.test();
 
   res.render('index.hbs', { title: 'Auto-Slacker' });
   
 });
+
+router.get('/list', function(req, res, next) {
+
+	console.log("Getting /list");
+
+  function* getNames(){
+  		console.log('Greetings from getNames!');
+		const chanNames = yield* AS.listChannels();
+		console.log("Greetings from getNames!",chanNames);
+		return chanNames;
+	};
+
+	const chanNames = getNames();
+	//getNames.next();
+	console.log("Got the chanNames!", chanNames);
+	const channels = Object.keys(AS.channels);
+	
+	res.render('index.hbs', {
+		title: 'Auto-Slacker',
+		 list: chanNames
+	});
+})
 
 module.exports = router;
